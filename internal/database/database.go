f009e6e5e958d3919e4b99d236caea7d82b24ab4
Maksym Bilan <maximb.mail@gmail.com>
2025-12-21 18:35:38 +0200

    Fixing issues

--- internal/database/database.go
+++ internal/database/database.go
@@ -214,25 +214,30 @@ type Transaction struct {
 }
 
 // GetTransactions retrieves transactions for an account (or all transactions if accountID is 0)
-// Transactions are entity types 37, 45, 46, 47, linked via ZACCOUNT2, using ZAMOUNT1
+// Transactions are entity types 37, 45, 46, 47, 43 (transfers), linked via ZACCOUNT2, using ZAMOUNT1
+// Dates are Core Data timestamps (seconds since 2001-01-01), converted to ISO format
 func (db *DB) GetTransactions(accountID int64, limit int) ([]Transaction, error) {
 	var query string
 	var args []interface{}
 
 	if accountID > 0 {
 		query = `
-			SELECT Z_PK, ZAMOUNT1, ZDATE1, ZDESC2, ZACCOUNT2
+			SELECT Z_PK, ZAMOUNT1, 
+				CASE WHEN ZDATE1 IS NOT NULL THEN datetime('2001-01-01', '+' || CAST(ZDATE1 AS INTEGER) || ' seconds') ELSE NULL END as transaction_date, 
+				ZDESC2, ZACCOUNT2
 			FROM ZSYNCOBJECT
-			WHERE Z_ENT IN (37, 45, 46, 47) AND ZAMOUNT1 IS NOT NULL AND ZACCOUNT2 = ?
+			WHERE Z_ENT IN (37, 45, 46, 47, 43) AND ZAMOUNT1 IS NOT NULL AND (ZACCOUNT2 = ? OR ZACCOUNT = ?)
 			ORDER BY ZDATE1 DESC
 			LIMIT ?
 		`
-		args = []interface{}{accountID, limit}
+		args = []interface{}{accountID, accountID, limit}
 	} else {
 		query = `
-			SELECT Z_PK, ZAMOUNT1, ZDATE1, ZDESC2, ZACCOUNT2
+			SELECT Z_PK, ZAMOUNT1, 
+				CASE WHEN ZDATE1 IS NOT NULL THEN datetime('2001-01-01', '+' || CAST(ZDATE1 AS INTEGER) || ' seconds') ELSE NULL END as transaction_date, 
+				ZDESC2, ZACCOUNT2
 			FROM ZSYNCOBJECT
-			WHERE Z_ENT IN (37, 45, 46, 47) AND ZAMOUNT1 IS NOT NULL
+			WHERE Z_ENT IN (37, 45, 46, 47, 43) AND ZAMOUNT1 IS NOT NULL
 			ORDER BY ZDATE1 DESC
 			LIMIT ?
 		`
